f01dbe106e913bf6dc85aba45ae9cb362b87a366
sahir <sahir@pop-os.localdomain>
2024-08-28 20:23:37 +0530

    chore: linking new sect

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import VideoScrollSection from './section/VideoScrollSection';
 import { HoverProvider } from './ContextProviders/HoverWrapperProvider';
+import ExpertiseSection from './section/ExpertiseSection';
  
 
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <HoverProvider>
       <VideoScrollSection />
+      <ExpertiseSection />
     </HoverProvider>
   );
 }
