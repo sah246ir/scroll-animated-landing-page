596e848e85d07363e08e8aab10b1fe1571b1fc2c
sahir <sahir@pop-os.localdomain>
2024-08-29 18:54:27 +0530

    feat: paragraph animation

---
+++ src/components/smartParagraph.tsx
@@ -0,0 +1,98 @@
+import { motion, MotionValue, useScroll, useTransform } from 'framer-motion'
+import { useRef } from 'react'
+
+const SmartParagraph = () => {
+    const element = useRef(null)
+    const { scrollYProgress } = useScroll({
+        target: element,
+        offset: ['start .9', 'start .25']
+    })
+
+    return (
+        <motion.p
+            ref={element}
+            className="text-[4ch] text-black text-left max-w-[54vw]"
+        >
+            <Sentence scrollYProgress={scrollYProgress} sentence='Wokine
+                conçoit
+                des
+                solutions
+                et
+                expériences
+                digitales
+                qui
+                allient
+                créativité
+                ,
+                technologie
+                et
+                engagement
+                éthique
+                .
+                ' 
+            />
+
+        </motion.p>
+    )
+}
+
+interface SentenceProps {
+    sentence: string,
+    scrollYProgress: MotionValue<number>
+}
+const Sentence = ({ sentence, scrollYProgress }: SentenceProps) => {
+    const words = sentence.split(" ")
+    return (
+        <span className='max-w-[54vw] inline-block'>
+            {words.map((word, i) => {
+                const start = i / words.length;
+                const end = start + (1 / words.length)
+                return (
+                    <Word scrollYProgress={scrollYProgress} range={[start, end]} key={i} word={word} />
+                )
+            })}
+        </span>
+    )
+}
+
+interface WordProps {
+    word: string
+    range: number[],
+    scrollYProgress: MotionValue<number>
+}
+const Word = ({ word, range, scrollYProgress }: WordProps) => {
+    const chars = word.split("")
+    const amount = range[1] - range[0]
+    const step = amount / word.length
+    console.log(step, amount, range)
+    return (
+        <span className='inline-block'>
+            {chars.map((char, i) => {
+                const start = range[0] + (step * i)
+                const end = range[0] + (step * (i + 1))
+                return (
+                    <Char scrollYProgress={scrollYProgress} range={[start, end]} key={i} char={char} />
+                )
+            })}
+            &nbsp;
+        </span>
+    )
+}
+
+interface CharProps {
+    char: string
+    range: number[],
+    scrollYProgress: MotionValue<number>
+}
+const Char = ({ char, range, scrollYProgress }: CharProps) => {
+    console.log(range)
+    const opacity = useTransform(scrollYProgress, range, [0, 1])
+    return (
+        <span className='relative'>
+            <span className='opacity-[0.3] absolute'>{char}</span>
+            <motion.span style={{ opacity }} className='transition duration-300'>{char}</motion.span>
+        </span>
+    )
+}
+
+export default SmartParagraph
