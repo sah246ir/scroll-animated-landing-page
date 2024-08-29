383b8c64b235219761880da6f11fe147ae32bb33
sahir <sahir@pop-os.localdomain>
2024-08-29 10:51:29 +0530

    style: fake padding

--- src/ContextProviders/HoverWrapperProvider.tsx
+++ src/ContextProviders/HoverWrapperProvider.tsx
@@ -12,20 +12,21 @@ export const HoverWrapperContext = createContext<HoverContextType | undefined>(u
 // Provider component
 export const HoverProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
   // State for managing the position and size of the background div
-  const [style, setStyle] = useState<React.CSSProperties>({});
+  const [style, setStyle] = useState<React.CSSProperties>({backgroundColor:"#DCF48E"});
 
   const updateWrapperPosition = (e: React.MouseEvent<HTMLElement, MouseEvent>) => {
     const target = e.currentTarget.getBoundingClientRect();
 
     // Calculate the position and size
-    const x = (target.left) + window.scrollX;
-    const y = (target.top) + window.scrollY;
-    const width = target.width ;
-    const height = target.height;
+    const x = (target.left-18) + window.scrollX;
+    const y = (target.top-6) + window.scrollY;
+    const width = target.width+36 ;
+    const height = target.height+12;
 
     // Update the style state
     e.currentTarget.classList.add("bg-opacity-0")
     setStyle({
+      ...style,
       opacity: 1,
       transform: `translate(${x}px, ${y}px)`,
       width: `${width}px`,
@@ -46,7 +47,7 @@ export const HoverProvider: React.FC<{ children: ReactNode }> = ({ children }) =
     <HoverWrapperContext.Provider value={{ updateWrapperPosition,onLeave }}>
       <div
         style={style}
-        className="absolute rounded-full bg-lime-300 z-0 pointer-events-none transition-all duration-300"
+        className="absolute rounded-full bg-lime-300 z-0 bg-opacity-80 pointer-events-none transition-all duration-300"
       />
       <div className="z-40">{children}</div>
     </HoverWrapperContext.Provider>
