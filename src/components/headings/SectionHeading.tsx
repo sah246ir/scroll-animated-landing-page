4d55e01b9d09b54df74d1c36b71aa3dd1277891b
sahir <sahir@pop-os.localdomain>
2024-08-30 17:55:15 +0530

    feat: section headinf

---
+++ src/components/headings/SectionHeading.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+interface PropTypes{
+    children:React.ReactNode
+    className?:string
+}
+const SectionHeading = ({children,className}:PropTypes) => {
+    return (
+        <div className='flex items-center gap-3'>
+            <div className='bg-black size-2 rounded-full  ' />
+            <p className={'text-sm ' + className}>{children}</p>
+        </div>
+    )
+}
+
+export default SectionHeading
