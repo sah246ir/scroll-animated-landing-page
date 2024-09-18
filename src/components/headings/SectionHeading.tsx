18444c955eaa2a854a666189ffbc64e30922d01e
sahir <sahir@pop-os.localdomain>
2024-09-18 21:18:09 +0530

    feat: bullet class prop

--- src/components/headings/SectionHeading.tsx
+++ src/components/headings/SectionHeading.tsx
@@ -3,12 +3,13 @@ import React from 'react'
 interface PropTypes{
     children:React.ReactNode
     className?:string
+    bulletClassName?:string
     bullet?:boolean
 }
-const SectionHeading = ({children,className,bullet=true}:PropTypes) => {
+const SectionHeading = ({children,className,bulletClassName,bullet=true}:PropTypes) => {
     return (
         <div className={'flex items-center gap-3 ' + className}>
-            {bullet && <div className='bg-black size-2 rounded-full  ' />}
+            {bullet && <div className={'bg-black size-2 rounded-full  ' + bulletClassName} />}
             <p className={'text-sm w-full '}>{children}</p>
         </div>
     )
