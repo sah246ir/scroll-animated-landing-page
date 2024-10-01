91c5c918ea05858dbf35aa9294ca8b99d4d7b1f3
sahir <sahir@pop-os.localdomain>
2024-10-01 14:44:07 +0530

    feat: navigate back

--- src/components/ComeUpPageWrapper.tsx
+++ src/components/ComeUpPageWrapper.tsx
@@ -1,11 +1,13 @@
 import { motion } from 'framer-motion'
 import { X } from 'lucide-react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 interface PropTypes{
     children:React.ReactNode
 }
 const ComeUpPageWrapper = ({children}:PropTypes) => {
+  const navigate = useNavigate();
   return (
     <main className='p-10 h-screen overflow-hidden'>
       <motion.section 
@@ -18,7 +20,7 @@ const ComeUpPageWrapper = ({children}:PropTypes) => {
         duration:'2'
       }}
       >
-        <button className='absolute right-3 top-3 bg-black w-12 h-12 rounded-full grid place-items-center'>
+        <button onClick={()=>navigate(-1)} className='absolute right-3 top-3 bg-black w-12 h-12 rounded-full grid place-items-center'>
             <X stroke='white'/>
         </button>
         {children}
