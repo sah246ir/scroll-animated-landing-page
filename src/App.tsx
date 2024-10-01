5fde012303f809e2ceefd39f7368d3ee766d6483
sahir <sahir@pop-os.localdomain>
2024-10-01 12:38:59 +0530

    feat: settingup redirection animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './page/Home';
+import { AnimatePresence } from 'framer-motion';
  
 function App() {
+  const location = useLocation()
   return (
     <>
       <Navbar />
-      <Routes>
-        <Route index element={<Home/>} />
-      </Routes>
+      <AnimatePresence mode="wait">
+
+        <Routes key={location.pathname} location={location}>
+          <Route index element={<Home/>} />
+        </Routes>
+      </AnimatePresence>
       
     </>
   );
