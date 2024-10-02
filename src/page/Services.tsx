5209cb356cc3473a617290738060e5a45ca91141
sahir <sahir@pop-os.localdomain>
2024-10-02 20:50:39 +0530

    feat: services popu0lated

--- src/page/Services.tsx
+++ src/page/Services.tsx
@@ -21,22 +21,46 @@ const Services = () => {
       <hr className='my-12 border-gray-300' />
 
       <Service
-        title='E-Learning'
-        description='With our E-learning services, we break down traditional walls of education. Learning is not confined to the four corners of a classroom or the pages of a textbook. We bring knowledge to life, reaching out visually to empower minds wherever they are.'
+        title='Illustration'
+        description="From print to digital, Clekk's team of talented illustrators creates stunning visuals that elevate your content and engage your audience. Whether it's for marketing materials, educational resources, or digital platforms, we bring visual brilliance to life. "
       />
 
       <hr className='my-12 border-gray-300' />
 
       <Service
-        title='E-Learning'
-        description='With our E-learning services, we break down traditional walls of education. Learning is not confined to the four corners of a classroom or the pages of a textbook. We bring knowledge to life, reaching out visually to empower minds wherever they are.'
+        title='Animations'
+        description="'With our E-learning services, we break down traditional walls of education. Learning is not confined to the four corners of a classroom or the pages of a textbook. We bring knowledge to life, reaching out visually to empower minds wherever they are.'"
       />
 
       <hr className='my-12 border-gray-300' />
 
       <Service
-        title='E-Learning'
-        description='With our E-learning services, we break down traditional walls of education. Learning is not confined to the four corners of a classroom or the pages of a textbook. We bring knowledge to life, reaching out visually to empower minds wherever they are.'
+        title='Creative Design'
+        description="Our expertise extends to meticulously crafting and laying out book pages for a seamless reading experience. We understand the importance of a well-designed layout in enhancing the reader's experience and conveying your message effectively. "
+      />
+      <hr className='my-12 border-gray-300' />
+
+      <Service
+        title='Translation'
+        description="At Clekkflix, we offer top-notch translation services to help you communicate effectively across languages. Whether you're a business expanding globally or an individual needing documents translated, we've got you covered."
+      />
+      <hr className='my-12 border-gray-300' />
+
+      <Service
+        title='Audio & Video'
+        description="Clekkflix offers professional audio and video services to enhance your content. We bring your vision to life with precision and creativity. With state-of-the-art equipment and industry expertise, we ensure your content stands out and leaves a lasting impression."
+      />
+      <hr className='my-12 border-gray-300' />
+
+      <Service
+        title='Corporate Presentations'
+        description="At ClekkFlix, our approach to corporate presentations transcends the traditional. We believe that a powerful presentation does more than just convey information—it inspires, engages, and leaves a lasting impression. Ensuring that your brand is showcased in a light that's not only seen but felt."
+      />
+      <hr className='my-12 border-gray-300' />
+
+      <Service
+        title='E-Publishing'
+        description="In E-Publishing, we transform the narrative. Books are no longer bound by covers but are liberated to exist in a digital realm. We take readers outside of traditional reading, into a world where stories are limitless and accessible everywhere. "
       />
 
 
