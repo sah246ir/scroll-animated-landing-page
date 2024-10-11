05f0397d512f69d8b207a2170523cd6d13d330b1
sahir <sahir@pop-os.localdomain>
2024-10-12 00:11:05 +0530

    feat: populating

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from './buttons/Button'
 import { ArrowRight } from 'lucide-react'
+import { Link } from 'react-router-dom'
 
 const logo = require("../media/logo.png")
 const Navbar = () => {
@@ -15,14 +16,16 @@ const Navbar = () => {
 
 
             <div className="flex gap-4 text-xs mt-4">
-                <button className='px-2 py-1 border rounded-full border-black  '> 
-                    some text&nbsp;
-                    <i className='inline'>
-                        <ArrowRight className='inline' size={15} />
-                    </i>
-                </button>
-                <Button>+91 xxx-xxx-xxxx</Button>
-                <Button>some text</Button>
+                <Link to={"/contact-us"}>
+                    <button className='px-2 py-1 border rounded-full border-black  '>
+                        Join us&nbsp;
+                        <i className='inline'>
+                            <ArrowRight className='inline' size={15} />
+                        </i>
+                    </button>
+                </Link>
+                <Link to={"tel:919920010885"}><Button>+91 99200 10885</Button></Link>
+                {/* <Button>some text</Button> */}
             </div>
              
         </nav>
