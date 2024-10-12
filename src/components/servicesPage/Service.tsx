d5752e7fec7f4d6a00f3c91d1937e613100ba624
sahir <sahir@pop-os.localdomain>
2024-10-12 12:50:14 +0530

    feat: href prop

--- src/components/servicesPage/Service.tsx
+++ src/components/servicesPage/Service.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import Button from '../buttons/Button'
+import { Link } from 'react-router-dom'
 
 interface PropTypes {
   title: string,
   description: string,
-  img: string
+  img: string,
+  href:string
 }
-const Service = ({ title, description, img }: PropTypes) => {
+const Service = ({ title, description, img ,href}: PropTypes) => {
   const immg = require(`../../media/services/${img}`) 
   return (
     <div className="flex justify-around items-center gap-4 max-w-[1500px] m-auto  ">
@@ -13,9 +16,12 @@ const Service = ({ title, description, img }: PropTypes) => {
         <h1 className='text-xl  text-gray-700 font-medium'>{title}</h1>
         <img className='w-[7.5vw] h-full' src={immg} alt="" />
       </div>
-      <p className='w-[65%] text-2xl font-normal' style={{ height: '100%' }}>
-        {description}
-      </p>
+      <div className='w-[65%]'>
+        <p className=' text-2xl font-normal' style={{ height: '100%' }}>
+          {description}
+        </p>
+        <Link to={href}><Button className='mt-3 text-orange-600'>View More</Button></Link>
+      </div>
     </div>
 
 
--- src/page/Services.tsx
+++ src/page/Services.tsx
@@ -16,86 +16,78 @@ const Services = () => {
 
       <hr className='my-12 border-gray-300' />
 
-      <Link to="/services/e-learning">
-        <Service
-          img='elearning1.gif'
-          title='E-Learning'
-          description="We’re redefining the way people learn with our E-learning services. Forget about the limits of traditional classrooms and textbooks—learning should be dynamic, engaging, and accessible from anywhere. Our immersive content brings subjects to life, empowering learners wherever they are and making education a powerful, interactive experience."
-        />
-      </Link>
+      <Service
+        href="/services/e-learning"
+        img='elearning1.gif'
+        title='E-Learning'
+        description="We’re redefining the way people learn with our E-learning services. Forget about the limits of traditional classrooms and textbooks—learning should be dynamic, engaging, and accessible from anywhere. Our immersive content brings subjects to life, empowering learners wherever they are and making education a powerful, interactive experience."
+      />
 
       <hr className='my-12 border-gray-300' />
 
-      <Link to="/services/illustration">
-        <Service
-          img='illustration1.gif'
-          title='Illustration'
-          description="Visual storytelling is our forte. At Clekk, our illustrators create stunning visuals that don’t just look good, but also connect with your audience. Whether it’s for marketing, educational content, or digital media, our illustrations go beyond aesthetics to bring your ideas to life in a way that captivates and communicates."
-        />
-      </Link>
+      <Service
+        href="/services/illustration"
+        img='illustration1.gif'
+        title='Illustration'
+        description="Visual storytelling is our forte. At Clekk, our illustrators create stunning visuals that don’t just look good, but also connect with your audience. Whether it’s for marketing, educational content, or digital media, our illustrations go beyond aesthetics to bring your ideas to life in a way that captivates and communicates."
+      />
 
       <hr className='my-12 border-gray-300' />
 
-      <Link to="/services/animation">
-        <Service
-          img='animation.gif'
-          title='Animations'
-          description="Why settle for static when you can make it move? Our animations take your ideas and transform them into dynamic, engaging visuals. From explaining complex concepts to telling compelling stories, our animations turn heads, grab attention, and make sure your message sticks."
-        />
-      </Link>
+      <Service
+        href="/services/animation"
+        img='animation.gif'
+        title='Animations'
+        description="Why settle for static when you can make it move? Our animations take your ideas and transform them into dynamic, engaging visuals. From explaining complex concepts to telling compelling stories, our animations turn heads, grab attention, and make sure your message sticks."
+      />
 
       <hr className='my-12 border-gray-300' />
 
-      <Link to="/services/creative-design">
-        <Service
-          img='creativedesign1.gif'
-          title='Creative Design'
-          description="Design is more than just making things look good—it’s about crafting a seamless experience. We design with precision, ensuring your content flows effortlessly and captures your audience’s attention. Whether it’s a book layout or a digital platform, we focus on delivering beauty and functionality in perfect balance."
-        />
-      </Link>
+      <Service
+        href="/services/creative-design"
+        img='creativedesign1.gif'
+        title='Creative Design'
+        description="Design is more than just making things look good—it’s about crafting a seamless experience. We design with precision, ensuring your content flows effortlessly and captures your audience’s attention. Whether it’s a book layout or a digital platform, we focus on delivering beauty and functionality in perfect balance."
+      />
 
       <hr className='my-12 border-gray-300' />
 
-      <Link to="/services/translation">
-        <Service
-          img='translation03.gif'
-          title='Translation'
-          description="Language shouldn’t be a barrier to success, and with ClekkFlix’s expert translation services, it won’t be. Whether you’re a business looking to go global or need important documents translated, we ensure your message is perfectly understood across any language and culture."
-        />
-      </Link>
+      <Service
+        href="/services/translation"
+        img='translation03.gif'
+        title='Translation'
+        description="Language shouldn’t be a barrier to success, and with ClekkFlix’s expert translation services, it won’t be. Whether you’re a business looking to go global or need important documents translated, we ensure your message is perfectly understood across any language and culture."
+      />
 
       <hr className='my-12 border-gray-300' />
 
-      <Link to="/services/audio-video">
-        <Service
-          img='audiovideo1.gif'
-          title='Audio & Video'
-          description="We bring your ideas to life through top-tier audio and video production. Our team knows how to take your vision and turn it into a polished product that resonates. Using cutting-edge equipment and creative flair, we produce content that not only stands out but also leaves a lasting impact on your audience."
-        />
-      </Link>
+      <Service
+        href="/services/audio-video"
+        img='audiovideo1.gif'
+        title='Audio & Video'
+        description="We bring your ideas to life through top-tier audio and video production. Our team knows how to take your vision and turn it into a polished product that resonates. Using cutting-edge equipment and creative flair, we produce content that not only stands out but also leaves a lasting impact on your audience."
+      />
 
       <hr className='my-12 border-gray-300' />
 
-      <Link to="/services/corporate-presentations">
-        <Service
-          img='corporatepresentation1.gif'
-          title='Corporate Presentations'
-          description="Presentations shouldn’t just convey information—they should inspire. At ClekkFlix, we design corporate presentations that do just that. We take your key messages and package them into compelling, visually engaging presentations that leave a strong, lasting impression on your audience."
-        />
-      </Link>
+      <Service
+        href="/services/corporate-presentations"
+        img='corporatepresentation1.gif'
+        title='Corporate Presentations'
+        description="Presentations shouldn’t just convey information—they should inspire. At ClekkFlix, we design corporate presentations that do just that. We take your key messages and package them into compelling, visually engaging presentations that leave a strong, lasting impression on your audience."
+      />
 
       <hr className='my-12 border-gray-300' />
 
-      <Link to="/services/e-publishing">
-        <Service
-          img='epublishing1.gif'
-          title='E-Publishing'
-          description="Step into the future of storytelling with our E-publishing services. Books no longer need to be limited by physical pages. We bring stories to the digital realm, creating interactive, accessible experiences that readers can engage with anytime, anywhere. Your stories become limitless."
-        />
-      </Link>
+      <Service
+        href="/services/e-publishing"
+        img='epublishing1.gif'
+        title='E-Publishing'
+        description="Step into the future of storytelling with our E-publishing services. Books no longer need to be limited by physical pages. We bring stories to the digital realm, creating interactive, accessible experiences that readers can engage with anytime, anywhere. Your stories become limitless."
+      />
 
       <div className='w-[65%] float-right px-5 max-w-[1500px]'>
-        <p className='mt-24 text-gray-400 text-2xl'>
+        <p className='mt-24 text-orange-300 text-2xl'>
           At ClekkFlix, we don’t just deliver services; we partner with you to drive real digital transformation. Working with talented agencies and companies, we’re focused on making an impact where it matters most, your success.
         </p>
 
