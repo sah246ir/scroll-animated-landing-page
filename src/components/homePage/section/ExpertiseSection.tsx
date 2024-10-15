f1742bd747607c484051eda9f4482922020e7f71
sahir <sahir@pop-os.localdomain>
2024-10-15 22:08:57 +0530

    fix: font sizes

--- src/components/homePage/section/ExpertiseSection.tsx
+++ src/components/homePage/section/ExpertiseSection.tsx
@@ -9,17 +9,17 @@ const ExpertiseSection = () => {
         <div className='py-20  bg-slate-10 bg-gray-800  text-white px-10 flex flex-col items-center text-center md:text-left md:flex-row md:items-start md:gap-28 gap-20 justify-around'>
             <div className='md:sticky top-20'>
                 <div className="sticky top-10 w-full md:max-w-[180px] flex flex-col gap-3">
-                    <SectionHeading className='justify-center md:justify-start [&>p]:text-4xl md:[&>p]:text-sm  '>
+                    <SectionHeading className='justify-center md:justify-start [&>p]:text-2xl md:[&>p]:text-sm  '>
                          Our Services 
                     </SectionHeading>
-                    <p className='text-2xl md:text-sm'>Services dedicated to enhancing your success</p>
+                    <p className='text-xl md:text-sm'>Services dedicated to enhancing your success</p>
                     <Link to={"/services"} className='mt-4 font-light text-2xl md:text-sm '>
                         <Button>View all services</Button>
                     </Link>
                 </div>
             </div>
-            <div className="text-[5xl] md:text-[3vw] gap-4 md:gap-3 sm:text-6xl tracking-wider group flex items-center md:items-start flex-col ">
-            <div className="text-[5xl] md:text-[3vw] gap-4 md:gap-3 sm:text-6xl tracking-wider group flex items-center md:items-start flex-col ">
+            <div className="text-4xl md:text-[3vw] gap-4 md:gap-3 sm:text-6xl tracking-wider group flex items-center md:items-start flex-col ">
+            <div className="text-4xl md:text-[3vw] gap-4 md:gap-3 sm:text-6xl tracking-wider group flex items-center md:items-start flex-col ">
       <Link to="/services/e-learning"><Expertise>E-learning</Expertise></Link>
       <Link to="/services/illustration"><Expertise>Illustration</Expertise></Link>
       <Link to="/services/animation"><Expertise>Animations</Expertise></Link>
